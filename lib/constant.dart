1cc402ba1073b17734be38c80bc83e7b8c418a59
Mirzaazmath <azmath9603399270@gmail.com>
2024-07-15 16:19:19 +0530

    first commit

---
+++ lib/constant.dart
@@ -0,0 +1,4 @@
+class AppConstant{
+  static const appId=914038978;
+  static const appSign="c4660b500c85d61571ca00e486be43e0b9530908f5ed8aaa6b8a2009c0e2fb90";
+}
---
+++ lib/presentation/call_screen.dart
@@ -0,0 +1,26 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+
+import '../constant.dart';
+
+final userId=Random().nextInt(9999);
+
+class CallPage extends StatelessWidget {
+  const CallPage({Key? key, required this.callID}) : super(key: key);
+  final String callID;
+
+  @override
+  Widget build(BuildContext context) {
+    return ZegoUIKitPrebuiltCall(
+      appID: AppConstant.appId, // Fill in the appID that you get from ZEGOCLOUD Admin Console.
+      appSign: AppConstant.appSign, // Fill in the appSign that you get from ZEGOCLOUD Admin Console.
+      userID: userId.toString(),
+      userName: 'UserName_$userId',
+      callID: callID,
+      // You can also use groupVideo/groupVoice/oneOnOneVoice to make more types of calls.
+      config: ZegoUIKitPrebuiltCallConfig.oneOnOneVideoCall(),
+    );
+  }
+}
